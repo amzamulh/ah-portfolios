fd7d7a30400be518775fee66bd732b61f3c3ef6c
Amzamul Haque <ahaquebd12@gmail.com>
2026-01-24 20:50:25 +0600

    add storybook for presentation ahui

--- packages/anzamhUI/vite.config.ts
+++ packages/anzamhUI/vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+/**
+ * Those for storybook
+ */
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@ah/anzamhui': path.resolve(__dirname, 'ahui/src'),
+    }
+  },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
+});
